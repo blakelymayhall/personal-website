0d8bf1da645bb0c1983a0d104bb8baa7e6d2d428
Blakely Mayhall <blakely.c.mayhall@gmail.com>
2024-07-20 16:18:44 -0500

    add profile picture blow up feature

---
+++ src/profilePictureOverlay.js
@@ -0,0 +1,41 @@
+const profilePictureOverlayFactory = () => {
+    // Data
+    //------------------------------------------------------------------------
+    let profilePictureOverlayActive = false;
+    const getProfilePictureOverlayActive = () => {
+        return profilePictureOverlayActive;
+    };
+
+    const docProfPicture = document.querySelector("#profPic");
+    //------------------------------------------------------------------------
+
+    // Support
+    //------------------------------------------------------------------------
+    const activate = () => {
+        profilePictureOverlayActive = true;
+        document.querySelector("#profPicOverlay").style.display = "flex";
+    }
+    //------------------------------------------------------------------------
+
+    // Events
+    //------------------------------------------------------------------------
+    document.querySelector("body").addEventListener("click", (e) => {
+        console.log(e.target);
+        if (profilePictureOverlayActive && e.target != docProfPicture) {
+            document.querySelector("#profPicOverlay").style.display = "none";
+            profilePictureOverlayActive = false;
+        }
+    });
+    //------------------------------------------------------------------------
+
+    // Export Functions
+    //------------------------------------------------------------------------
+    return {
+        activate,
+        getProfilePictureOverlayActive,
+    };
+    //------------------------------------------------------------------------
+
+};
+
+export {profilePictureOverlayFactory}
