835e9ad422fa2cb2dca54d658fb6886bdda34db1
Blakely Mayhall <blakely.c.mayhall@gmail.com>
2024-08-04 19:12:14 -0500

    revamp mbile support and responsiveness

--- src/init_page_load.js
+++ src/init_page_load.js
@@ -7,6 +7,14 @@ const initPageLoad = (isMobile) => {
         document.querySelector("#mobileNavBar").style.display = "flex";
         document.querySelector("#mobileHomePageContent").style.display = "block";
         document.querySelector("#desktopHomePageContent").style.display = "none";
+    } else {
+        document.querySelector("#sideBar").style.display = "flex";
+        document.querySelector("#headerBar").style.display = "flex";
+        document.querySelector("#mainContainer").style.display = "grid";
+        document.querySelector("#mobileHeaderBar").style.display = "none";
+        document.querySelector("#mobileNavBar").style.display = "none";
+        document.querySelector("#mobileHomePageContent").style.display = "none";
+        document.querySelector("#desktopHomePageContent").style.display = "block";
     }
 };
 
