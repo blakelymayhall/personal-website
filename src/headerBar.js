a41ff2c682454421ebdb80defa2cd18d231a5a71
Blakely Mayhall <blakely.c.mayhall@gmail.com>
2024-08-12 21:14:28 -0500

    code cleanup

--- src/headerBar.js
+++ src/headerBar.js
@@ -13,17 +13,7 @@ const headerBarFactory = () => {
 
     // Events
     //------------------------------------------------------------------------
-    // documentMenuOptions.forEach((documentMenuOption) => {
-    //     documentMenuOption.addEventListener("click", () => {
-    //         activeMenuOption = `#${documentMenuOption.id}`;
-    //         applyActiveMenuOptionDecor(activeMenuOption);
-    //     });
-    // });
 
-    // const profPicture = document.querySelector("#profPic");
-    // profPicture.addEventListener("click", () => {
-    //     profilePictureOverlay.activate();
-    // });
     //------------------------------------------------------------------------
 
     return {
